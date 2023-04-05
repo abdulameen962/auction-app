fb87fae5a3682589a4282309a0f7b2e73644aa49
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-05 15:44:32 +0100

    heroku procfile

--- commerce/settings.py
+++ commerce/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'auctions',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -129,4 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR,"/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
